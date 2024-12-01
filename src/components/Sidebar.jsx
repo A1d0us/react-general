3c27d570cf9d5b9b63f9f2627b114c7a0bd68126
a1d0us <diakonenko.vanya@gmail.com>
2024-12-01 15:39:46 +0100

    created navigation

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,28 @@
+import Link from "./Link";
+
+const Sidebar = () => {
+  const links = [
+    {label: 'Dropdown', path: '/dropdown'},
+    {label: 'Accordion', path: '/accordion'},
+    {label: 'Buttons', path: '/buttons'},
+  ];
+
+  const renderedLinks = links.map((link) => {
+    return <Link
+      className="mb-3"
+      to={link.path}
+      key={link.label}
+      activeClassName="font-bold border-l-4 border-blue-500 pl-2"
+    >
+      {link.label}
+    </Link>
+  })
+
+  return (
+    <div className="sticky top-0 overflow-y-scroll flex flex-col items-start">
+      {renderedLinks}
+    </div>
+  );
+}
+
+export default Sidebar;
